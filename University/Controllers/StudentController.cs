6c38b39e60b277cab582bb5f0edb4b4c241382cf
Jarzembowski <jargemboski@gmail.com>
2017-07-12 11:38:13 -0300

    alterações 2

--- University/Controllers/StudentController.cs
+++ University/Controllers/StudentController.cs
@@ -42,25 +42,30 @@ namespace University.Controllers
             return View();
         }
 
-        // POST: Students/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
+      {
+         try
+         {
             if (ModelState.IsValid)
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+               db.Students.Add(student);
+               db.SaveChanges();
+               return RedirectToAction("Index");
             }
+         }
+         catch (DataException /* dex */)
+         {
+            //Log the error (uncomment dex variable name and add a line here to write a log.
+            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+         }
+         return View(student);
+      }
 
-            return View(student);
-        }
 
-        // GET: Students/Edit/5
-        public ActionResult Edit(int? id)
+      // GET: Students/Edit/5
+      public ActionResult Edit(int? id)
         {
             if (id == null)
             {
@@ -74,6 +79,8 @@ namespace University.Controllers
             return View(student);
         }
 
+
+         /*
         // POST: Students/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -89,34 +96,77 @@ namespace University.Controllers
             }
             return View(student);
         }
+        */
 
-        // GET: Students/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
 
-        // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+      [HttpPost, ActionName("Edit")]
+      [ValidateAntiForgeryToken]
+      public ActionResult EditPost(int? id)
+      {
+         if (id == null)
+         {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         var studentToUpdate = db.Students.Find(id);
+         if (TryUpdateModel(studentToUpdate, "",
+            new string[] { "LastName", "FirstMidName", "EnrollmentDate" }))
+         {
+            try
+            {
+               db.SaveChanges();
+
+               return RedirectToAction("Index");
+            }
+            catch (DataException /* dex */)
+            {
+               //Log the error (uncomment dex variable name and add a line here to write a log.
+               ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+         }
+         return View(studentToUpdate);
+      }
+
+
+
+      // GET: Students/Delete/5
+      public ActionResult Delete(int? id, bool? saveChangesError = false)
+      {
+         if (id == null)
+         {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         if (saveChangesError.GetValueOrDefault())
+         {
+            ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+         }
+         Student student = db.Students.Find(id);
+         if (student == null)
+         {
+            return HttpNotFound();
+         }
+         return View(student);
+      }
+
+      // POST: Students/Delete/5
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Delete(int id)
+      {
+         try
+         {
             Student student = db.Students.Find(id);
             db.Students.Remove(student);
             db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+         }
+         catch (DataException/* dex */)
+         {
+            //Log the error (uncomment dex variable name and add a line here to write a log.
+            return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+         }
+         return RedirectToAction("Index");
+      }
 
-        protected override void Dispose(bool disposing)
+      protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
