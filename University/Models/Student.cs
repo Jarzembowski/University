2ecf602af3e3c5cfb6751b9cc57ab1238761e54c
Jarzembowski <jargemboski@gmail.com>
2017-07-14 15:59:55 -0300

    alterações 14-07

--- University/Models/Student.cs
+++ University/Models/Student.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace University.Models
 {
    public class Student
    {
       public int ID { get; set; }
-      public string LastName { get; set; }
-      public string FirstMidName { get; set; }
-      public DateTime EnrollmentDate { get; set; }
 
+      [StringLength(50)]
+      public string LastName { get; set; }
+
+      [StringLength(50)]
+      public string FirstMidName { get; set; }
+
+      [DataType(DataType.Date)]
+      [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+      public DateTime EnrollmentDate { get; set; }
       public virtual ICollection<Enrollment> Enrollments { get; set; }
    }
 
