8b6dc22b993d66ee22e1e58c3cd1350f6d94c552
Jarzembowski <jargemboski@gmail.com>
2017-08-01 11:41:13 -0300

    alterações 01-08

--- University/DAL/SchoolContext.cs
+++ University/DAL/SchoolContext.cs
@@ -14,9 +14,12 @@ namespace University.DAL
       {
       }
 
-      public DbSet<Student> Students { get; set; }
-      public DbSet<Enrollment> Enrollments { get; set; }
       public DbSet<Course> Courses { get; set; }
+      public DbSet<Department> Departments { get; set; }
+      public DbSet<Enrollment> Enrollments { get; set; }
+      public DbSet<Instructor> Instructors { get; set; }
+      public DbSet<Student> Students { get; set; }
+      public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
       protected override void OnModelCreating(DbModelBuilder modelBuilder)
       {
